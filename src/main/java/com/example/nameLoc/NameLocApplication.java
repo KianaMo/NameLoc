5f32b1a8da6fe5eca214f474d2892d7d2f198faa
KianaMo <kiana.montazeri@aalto.fi>
2023-01-24 00:03:36 +0200

    First exercise commit

---
+++ src/main/java/com/example/nameLoc/NameLocApplication.java
@@ -0,0 +1,13 @@
+package com.example.nameLoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NameLocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NameLocApplication.class, args);
+	}
+
+}
